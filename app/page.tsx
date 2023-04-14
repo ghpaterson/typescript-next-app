c59c8a587e3ad4a20e6cc2d69e7268ed5d0a78cb
Graeme Paterson <ghpaterson@gmail.com>
2023-04-14 14:48:37 +0100

    error ?Json not being called from api endpoint correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link"
 
-export default function Home() {
+async function getPosts(){
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
+  if(!res.ok){
+    console.log(res)
+  }
+  return res.json()
+  
+}
+
+export default async function Home() {
+  const data = await getPosts()
+  console.log(data)
   return (
     <main className="py-8 px-48">
      <Link 
